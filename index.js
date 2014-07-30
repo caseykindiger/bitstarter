c4ecbbbf4aaf4f649691350c1bc472b0b5d557b4
Ubuntu <ubuntu@ip-172-31-34-52.ec2.internal>
2014-07-30 16:17:35 +0000

    Created index.html and updated index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 var express = require('express')
 var app = express();
+var buffer = new Buffer(16);
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+buffer = readFileSync("index.html");
+
 app.get('/', function(request, response) {
-  response.send('Hello World2!')
+  response.send(buffer.toString())
 })
 
 app.listen(app.get('port'), function() {
