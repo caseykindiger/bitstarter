a0763827d9c0b52f189935a9c04b90b96bc3eb04
Ubuntu <ubuntu@ip-172-31-34-52.ec2.internal>
2014-07-30 16:35:13 +0000

    updated index.js to fix file system issues

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
-var express = require('express')
+var express = require('express');
 var app = express();
+var fs = require('fs');
 var buffer = new Buffer(16);
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
-buffer = readFileSync("index.html");
+buffer = fs.readFileSync('index.html');
 
 app.get('/', function(request, response) {
   response.send(buffer.toString())
